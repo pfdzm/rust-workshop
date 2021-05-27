7dbca9c849477d272ab5b80555547643ddfaefba
pfdzm <pfdzm@users.noreply.github.com>
2021-05-27 12:26:00 +0200

    Play around with Structs and Traits

--- part2/src/main.rs
+++ part2/src/main.rs
@@ -1,7 +1,25 @@
+use std::fmt;
+
 struct City {
     description: String,
     residents: u64,
     is_coastal: bool,
+}
+
+struct Point {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(x: {}, y: {}, z: {})", self.x, self.y, self.z)
+    }
+}
+
+fn new_point(x: f32, y: f32, z: f32) -> Point {
+    Point { x, y, z }
 }
 
 fn new_city(residents: u64, is_coastal: bool) -> City {
@@ -25,10 +43,13 @@ fn new_city(residents: u64, is_coastal: bool) -> City {
 
 fn main() {
     let rustville: City = new_city(133742069, false);
+    let point_a = new_point(12.1, 12.33, 0.2);
 
     println!("This city can be described as: {}", rustville.description);
 
     println!("This city has {} residents", rustville.residents);
+
+    println!("Your point is {}", point_a);
 
     if rustville.is_coastal {
         println!("It is a coastal city.");
